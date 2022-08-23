7023e5a707263d31f23ee79cab8e5f5a2cf6735e
2001monika <105417181+2001monika@users.noreply.github.com>
2022-08-23 18:11:25 +0530

    Add files via upload

---
+++ mul using fun.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp1
+//{
+//    internal class fun
+//    {
+//        public static int Mult(int a,int b,int c)
+//        {
+//            int mul;
+//            mul = a * b * c;
+//            return mul;
+//        }
+//        public static void Main()
+//        {
+//            int a, b, c;
+//            Console.WriteLine("enter a number");
+//            a = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("enter b number");
+//            b = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("enter c number");
+//            c = Convert.ToInt32(Console.ReadLine());
+//            int res = Mult(a, b, c);
+
+//            Console.WriteLine($"Product of three number is {res} ");
+//        }
+//    }
+
+//}
