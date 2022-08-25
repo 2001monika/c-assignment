cc0acfd92d6ae1731064abffbc6000366cc2f22f
2001monika <105417181+2001monika@users.noreply.github.com>
2022-08-25 18:22:42 +0530

    Add files via upload

---
+++ copy_ele_from_onearray_to_ another.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace assingments
+//{
+//    internal class copy_ele_from_onearray_to__another
+//    {
+//        public static void Main()
+//        {
+//            int[] arrSource = new int[4] { 1, 2, 3, 4 };
+//            int[] arrTarget = new int[2];
+//            Array.Copy(arrSource, arrTarget, 2);
+//            Console.WriteLine("destination array....");
+//            foreach (int value in arrTarget)
+//            {
+//                Console.WriteLine(value);
+//            }
+//        }
+//    }
+//}
